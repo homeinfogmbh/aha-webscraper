ff01ada37b57f125775ff7386f815a2265281eb5
Richard Neumann <r.neumann@homeinfo.de>
2017-09-25 16:00:14 +0200

    Updated dependecies.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='aha',
     version='latest',
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    requires=['bs4', 'requests'],
+    requires=['requests', 'bs4', 'html5lib'],
     py_modules=['aha'],
     description=(
         'AHA Region Hannover garbage collection dates web scraping API.'))
